d838285df23d26d400886a2d4429460f230f5f0b
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-02-21 20:35:17 +0500

    Added line for the completion

--- script.js
+++ script.js
@@ -14,19 +14,21 @@ const changeTurn = () => {
 const checkWin = ()=>{
     let boxTexts = document.getElementsByClassName('boxText');
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0,1,2,3,3,0],
+        [3,4,5,3,11,0],
+        [6,7,8,3,18,0],
+        [0,3,6,-4.5,10,90],
+        [1,4,7,3,10,90],
+        [2,5,8,10,10,90],
+        [0,4,8,3,11,45],
+        [2,4,6,2,11,135]
     ];
     wins.forEach((e)=>{
         if((boxTexts[e[0]].innerText === boxTexts[e[1]].innerText)&&(boxTexts[e[1]].innerText === boxTexts[e[2]].innerText)&&(boxTexts[e[0]].innerText!=="")){
             document.querySelector(".info").innerText = boxTexts[e[0]].innerText + " Won";
             gameOver = true;
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector(".line").style.width = "29vh";
         }
     })
 }
